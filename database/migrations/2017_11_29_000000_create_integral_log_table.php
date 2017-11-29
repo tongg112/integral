345297fd1d0de21912d6e1e1a8b750febe1912d3
xtong <lotongok@gmail.com>
2017-11-29 17:23:14 +0800

    增加积分记录表

---
+++ database/migrations/2017_11_29_000000_create_integral_log_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIntegralTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('integral_log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('uid')->unique();
+            $table->integer('changes');
+            $table->string('types');
+            $table->string('remark');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('integral_log');
+    }
+}
